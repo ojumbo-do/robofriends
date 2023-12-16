c5111fbb8df15d9c72b1eb1f051f6b955f4f8b72
Dancun Ojumbo <70913596+klaus990@users.noreply.github.com>
2023-12-16 15:45:38 +0300

    Adding Font and Style to the logo

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 const Card = ({ name, email, id }) => {
   return (
     <div className="bg-light-green dib br3 pa2 ma2 grow bw3 shadow tc">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <img src={`https://robohash.org/${id}?200x200`} alt={`robot-${id}`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
