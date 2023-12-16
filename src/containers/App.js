96bbc3c850dc6b8e3ce63c9139074ec734b3d6de
Dancun Ojumbo <70913596+klaus990@users.noreply.github.com>
2023-12-16 17:18:43 +0300

    Beautified file structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList.js";
+import SearchBox from "../components/SearchBox.js";
 import "./App.css";
-import Scroll from "./Scroll.js";
+import Scroll from "../components/Scroll.js";
 
 class App extends Component {
   constructor() {
